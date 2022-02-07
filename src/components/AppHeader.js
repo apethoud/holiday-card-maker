b093f2385c2a74ac656d2c09cc47edf96ec7fdef
Andrew Pethoud <andrewpethoud@gmail.com>
2022-02-07 13:46:41 -0800

    stub out header links

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -3,8 +3,10 @@ import { ThemeContext } from "../contexts/ThemeContext";
 import {
   Header,
   HeaderText,
+  HeaderLink,
   SmallButton
 } from "./StyledComponents";
+import { Link } from 'react-router-dom';
 
 export default function AppHeader(props) {
   const { toggleTheme } = useContext(ThemeContext);
@@ -13,6 +15,14 @@ export default function AppHeader(props) {
     <Header>
       <HeaderText>Holiday Card Maker</HeaderText>
       <SmallButton onClick={toggleTheme}>Toggle Theme</SmallButton>
+      <div className="Flex">
+        <Link to="/">
+          <HeaderLink>Home</HeaderLink>
+        </Link>
+        <Link to="/card-builder">
+          <HeaderLink>Card Builder</HeaderLink>
+        </Link>
+      </div>
     </Header>
   );
 }
